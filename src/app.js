0cc022e235b324ed7096111f2b55b54c5c4db599
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-08-30 08:33:33 -0500

    feat: add winston config and use as a middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import router from './routers/pizza.router.js'
 import mongoose from 'mongoose'
+import { loggerMiddleware } from './utils.js'
 
 const app = express()
+app.use(loggerMiddleware)
 app.use('/api/pizzas', router)
 
 mongoose.connect('mongodb://localhost:27017', { dbName: 'integradora_4'})

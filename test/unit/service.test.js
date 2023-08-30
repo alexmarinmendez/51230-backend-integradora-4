71ff4fea6eac4cb74cff698fc6f5f23fdb83f08a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-08-30 09:18:18 -0500

    feat: add second unit test

--- test/unit/service.test.js
+++ test/unit/service.test.js
@@ -7,8 +7,23 @@ const service = new PizzaService()
 const expect = chai.expect
 
 describe('Test CRUD of Pizza Service',  () => {
+    before(async function() {
+        try {
+            await mongoose.connection.collections.pizzas.drop()
+        } catch(err) {
+            console.log(err.message)
+        }
+    })
     it('Must return all pizzas', async () => {
         const result = await service.get()
         expect(result).to.be.deep.equal([])
+    })
+    it('Must create a pizza', async () => {
+        const result = await service.create({
+            name: 'Fugazzeta',
+            size: 'medium',
+            price: 100
+        })
+        expect(result).to.have.property('_id')
     })
 })
